2a5b93a96589b98a81c4529ce974adf3978f1199
arturovirreyiii <78330717+arturovirreyiii@users.noreply.github.com>
2021-02-02 00:17:21 -0700

    Add files via upload
    1) Database_creating (code used to create my small, custom, database project)
    2) Locating_Format (format used to define my table schema including the customer's member id, name, movie picked, and timestamp when rented)
    3) Insert_Values1 (first set of values entered in for one customer example)
    4) Insert_Values2 (additional set of values entered in for additional customers)

---
+++ Insert_Values1.sql
@@ -0,0 +1,3 @@
+INSERT INTO DBProject (Memberid, name, movie_rental, rented)
+VALUES
+('1','john smith','Star Wars III',CURRENT_TIMESTAMP);
---
+++ Insert_Values2.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbproject (memberid, name, movie_rental, rented)
+VALUES ('2','bill conner','Gladiator',CURRENT_TIMESTAMP),
+('3','delilah mcbride','pitch perfect',CURRENT_TIMESTAMP),
+('4','hailey mcbride','E.T.',CURRENT_TIMESTAMP),
+('5','brett yale','The Hobbit',CURRENT_TIMESTAMP);
